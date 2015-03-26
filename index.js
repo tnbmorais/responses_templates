015c40f9be66e667c1534e237c7b399e684027d1
tnbmorais <tnbmorais@hotmail.com>
2015-03-26 22:56:57 +0000

    Add index and template markets json

---
+++ index.js
@@ -0,0 +1,26 @@
+var mustache = require('mustache');
+var marketsTemplate = require('./template.json');
+
+console.log(marketsTemplate);
+
+var baseResponse = {
+    facets: [],
+    attachments: [],
+    results: []
+};
+
+function constructResponse(obj) {
+    var marketInformation = mustache.render(JSON.stringify(marketsTemplate), obj);
+    baseResponse.facets.push(JSON.parse(marketInformation));
+}
+
+/****************** CENAS **********************/
+
+var obj = {
+    marketId: 'xpto',
+    eventTypeId: 10
+};
+
+constructResponse(obj);
+
+console.log(JSON.stringify(baseResponse));
