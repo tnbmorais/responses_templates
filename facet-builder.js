90b873985bf5faeb2f9e5222f8ee1be4505c65bf
moraist <tiago.morais@blip.pt>
2015-04-06 16:23:16 +0100

    Restructure attachments builder

--- facet-builder.js
+++ facet-builder.js
@@ -1,77 +1,77 @@
 var mustache = require('mustache');
 var templatesLocation = './templates/facet/';
 
-function buildLitemarketsAttachments(baseResponse, litemarkets) {
-
-    var marketInformation;
-    var marketLiteTemplate;
-
-    if (!litemarkets || litemarkets.length < 1) {
-        return;
-    }
-
-    marketLiteTemplate = require(templatesLocation + 'marketlite-template.json');
-
-    baseResponse.attachments.liteMarkets = {};
-
-    litemarkets.forEach(function (market) {
-        if (!market.marketId) {
-            return;
-        }
-        marketInformation = JSON.parse(mustache.render(JSON.stringify(marketLiteTemplate), market));
-        baseResponse.attachments.liteMarkets[market.marketId] = marketInformation;
-    });
-}
-
-
-function buildCompetitionsAttachments(baseResponse, competitions) {
-
-    var competitionInformation;
-    var competitionTemplate;
-
-    if (!competitions || competitions.length < 1) {
-        return;
-    }
-
-    competitionTemplate = require(templatesLocation + 'competition-template.json');
-
-    baseResponse.attachments.competitions = {};
-
-    competitions.forEach(function (competition) {
-        if (!competition.competitionId) {
-            return;
-        }
-        competitionInformation = JSON.parse(mustache.render(JSON.stringify(competitionTemplate), competition));
-        baseResponse.attachments.competitions[competition.competitionId] = competitionInformation;
-    });
-
-}
-
-
-function buildEventsAttachments(baseResponse, events) {
-
-    var eventInformation;
-    var eventTemplate;
-
-    if (!events || events.length < 1) {
-        return;
-    }
-
-    eventTemplate = require(templatesLocation + 'event-template.json');
-
-    baseResponse.attachments.events = {};
-
-    events.forEach(function (event) {
-        if (!event.eventId) {
-            return;
-        }
-        eventInformation = JSON.parse(mustache.render(JSON.stringify(eventTemplate), event));
-        baseResponse.attachments.events[event.eventId] = eventInformation;
-    });
-}
-
 
 function buildAttachments(baseResponse, attachments) {
+
+    function buildLitemarketsAttachments(baseResponse, litemarkets) {
+
+        var marketInformation;
+        var marketLiteTemplate;
+
+        if (!litemarkets || litemarkets.length < 1) {
+            return;
+        }
+
+        marketLiteTemplate = require(templatesLocation + 'marketlite-template.json');
+
+        baseResponse.attachments.liteMarkets = {};
+
+        litemarkets.forEach(function (market) {
+            if (!market.marketId) {
+                return;
+            }
+            marketInformation = JSON.parse(mustache.render(JSON.stringify(marketLiteTemplate), market));
+            baseResponse.attachments.liteMarkets[market.marketId] = marketInformation;
+        });
+    }
+
+
+    function buildCompetitionsAttachments(baseResponse, competitions) {
+
+        var competitionInformation;
+        var competitionTemplate;
+
+        if (!competitions || competitions.length < 1) {
+            return;
+        }
+
+        competitionTemplate = require(templatesLocation + 'competition-template.json');
+
+        baseResponse.attachments.competitions = {};
+
+        competitions.forEach(function (competition) {
+            if (!competition.competitionId) {
+                return;
+            }
+            competitionInformation = JSON.parse(mustache.render(JSON.stringify(competitionTemplate), competition));
+            baseResponse.attachments.competitions[competition.competitionId] = competitionInformation;
+        });
+
+    }
+
+
+    function buildEventsAttachments(baseResponse, events) {
+
+        var eventInformation;
+        var eventTemplate;
+
+        if (!events || events.length < 1) {
+            return;
+        }
+
+        eventTemplate = require(templatesLocation + 'event-template.json');
+
+        baseResponse.attachments.events = {};
+
+        events.forEach(function (event) {
+            if (!event.eventId) {
+                return;
+            }
+            eventInformation = JSON.parse(mustache.render(JSON.stringify(eventTemplate), event));
+            baseResponse.attachments.events[event.eventId] = eventInformation;
+        });
+    }
 
     // Competitions attachments
     buildCompetitionsAttachments(baseResponse, attachments.competitions);
